b600c31263b46d523f360306814513516df3ade1
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2022-10-17 16:55:57 -0500

    Update FunctionExtensions.py

--- pyminlib/FunctionExtensions.py
+++ pyminlib/FunctionExtensions.py
@@ -1,6 +1,7 @@
 class lists:
    def splice(a, b:int, c:int="", d:list=(())):
       """
+      ActionScript Array.splice()
       a is a list. (required)
       b is the element to start at (refers to the first element as 1 instead of 0). (required)
       c is the number of elements to remove (including b). If c is not provided, all elements from b on will be removed. (optional)
@@ -16,13 +17,3 @@ class lists:
          for i in range(0, len(d)):
             templist.insert(b+i, d[i])
       return templist
-
-l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-l2 = list(("a", "b", "c"))
-print(lists.splice(l1, 3, 5))
-l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-print(lists.splice(l1, 3))
-l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-l2 = list(("a", "b", "c"))
-print(lists.splice(l1, 3, 5, l2))
-#string.test()
