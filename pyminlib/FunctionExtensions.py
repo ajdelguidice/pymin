f83d2bd70aa4e0d88cc5c3529ee135a078e5de21
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2022-10-17 16:57:15 -0500

    Update FunctionExtensions.py

--- pyminlib/FunctionExtensions.py
+++ pyminlib/FunctionExtensions.py
@@ -5,7 +5,7 @@ class lists:
       a is a list. (required)
       b is the element to start at (refers to the first element as 1 instead of 0). (required)
       c is the number of elements to remove (including b). If c is not provided, all elements from b on will be removed. (optional)
-      d is a list of elements to be added at b after the splice. (optional)
+      d is a list of elements to be added at b after b to b+c is deleted. (optional)
       """
       templist = a
       if c == "":
