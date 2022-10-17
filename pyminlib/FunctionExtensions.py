56999055550e49f404942169f45a367aa438aece
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2022-10-17 16:42:31 -0500

    Add files via upload

--- pyminlib/FunctionExtensions.py
+++ pyminlib/FunctionExtensions.py
@@ -1,17 +1,29 @@
-class string:
-   def splice(a, b, c=""):
+class lists:
+   def splice(a, b:int, c:int="", d:list=(())):
+      """
+      Splice a starting at b, ending at c, and stepping by d
+      a is a list. (required)
+      b is the element to start at (refers to the first element as 1 instead of 0). (required)
+      c is the number of elements to remove (including b). If c is not provided, all elements from b on will be removed. (optional)
+      d is a list of elements to be added at b after the splice. (optional)
+      """
       templist = a
       if c == "":
          del templist[b:]
       elif c != 0:
          for i in range(c, 0, -1):
             templist.pop(b+i-1)
+      if d != []:
+         for i in range(0, len(d)):
+            templist.insert(b+i, d[i])
       return templist
-#      tempArray2 = tempArray1[b:b+c]
-#      tempArray3 = 
-#      return tempArray2
 
 l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-print(string.splice(l1, 3, 5))
-print(string.splice(l1, 3))
+l2 = list(("a", "b", "c"))
+print(lists.splice(l1, 3, 5))
+l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print(lists.splice(l1, 3))
+l1 = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+l2 = list(("a", "b", "c"))
+print(lists.splice(l1, 3, 5, l2))
 #string.test()
