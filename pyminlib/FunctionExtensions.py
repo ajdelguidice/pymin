d489f16a56e8a6c2c33a1cfa22bbfe9029ece37a
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2023-01-25 17:11:57 -0600

    Add files via upload

--- pyminlib/FunctionExtensions.py
+++ pyminlib/FunctionExtensions.py
@@ -1,5 +1,7 @@
+import math
+
 class lists:
-   def splice(a, b:int, c:int="", d:list=(())):
+   def splice(a:list, b:int, c:int="", d:list=[]):
       """
       ActionScript Array.splice()
       a is a list. (required)
@@ -17,3 +19,71 @@ class lists:
          for i in range(0, len(d)):
             templist.insert(b+i, d[i])
       return templist
+
+class logic:
+   def inv(a:bool):
+      "Returns the inverse of a boolean"
+      if (a == True):
+         return False
+      if (a == False):
+         return True
+   def land(a:bool, b:bool):
+      "Logical and of two booleans"
+      if (a == True) and (b == True):
+         return True
+      else:
+         return False
+   def lor(a:bool, b:bool):
+      "Logical or of two booleans"
+      if (a == True) or (b == True):
+         return True
+      else:
+         return False
+   def nand(a:bool, b:bool):
+      "Logical nand of two booleans"
+      c = logic.inv(logic.land(a, b))
+      return c
+   def nor(a:bool, b:bool):
+      "Logical nor of two booleans"
+      c = logic.inv(logic.lor(a, b))
+      return c
+   def xor(a:bool, b:bool):
+      "Logical xor of two booleans"
+      c = logic.land(logic.nand(a, b), logic.lor(a, b))
+      return c
+   def xnor(a:bool, b:bool):
+      "Logical xnor of two booleans"
+      c = logic.lor(logic.nand(a, b), logic.land(a, b))
+      return c
+
+class operations:
+   def roundup(a:float, b:float=""):
+      """
+      rounds a up to every b.
+      if b is not provided, assumes b is 10
+      """
+      if b == "":
+         b = 10
+      c = a / b
+      d = math.ceil(c)
+      e = d * b
+      return e
+   def rounddown(a:float, b:float=""):
+      """
+      rounds a down to every b.
+      if b is not provided, assumes b is 10
+      """
+      if b == "":
+         b = 10
+      c = a / b
+      d = math.floor(c)
+      e = d * b
+      return e
+   def plusplus1(a):
+      b = a
+      b += 1
+      return b
+   #def plusplus2(a, b:str):
+   #   global str(b)
+   #   str(b) += 1
+   #   return a
