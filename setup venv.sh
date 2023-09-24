50d4f1457632877de4401e734cfc868fcaa9f70f
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2023-09-24 16:34:16 -0500

    fixed venv scripts

--- setup venv.sh
+++ setup venv.sh
@@ -4,15 +4,21 @@ read -p "Major version (X): " maversion
 if ! [[ $maversion =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
-read -p "Minor version (Y): " miversion
-if ! [[ $miversion =~ $re ]] ; then
-   echo "error: Not a number" >&2; exit 1
+read -p "Do you have multiple versions installed?(y/n) " mulversion
+if [[ $mulversion == "n" ]] ; then
+   fullversion=$maversion
+elif [[ $mulversion == "y" ]] ; then
+   read -p "Minor version (Y): " miversion
+   if ! [[ $miversion =~ $re ]] ; then
+      echo "error: Not a number" >&2; exit 1
+   fi
+   fullversion=$maversion"."$miversion
 fi
-fullversion=$maversion"."$miversion
-python$maversion -m venv ./Pymin-venv/
+python$fullversion -m venv ./Pymin-venv/
 mkdir ./Pymin-venv/Pymin/
 cp ./Pymin.py ./Pymin-venv/Pymin/Pymin.py
 chmod +x ./Pymin-venv/Pymin/Pymin.py
 source ./Pymin-venv/bin/activate
-pip install Mini-AMF tkhtmlview numpy Pillow as3lib
+pip install Mini-AMF tkhtmlview numpy Pillow as3lib mss
+pip install -U Mini-AMF tkhtmlview numpy Pillow as3lib mss
 deactivate
