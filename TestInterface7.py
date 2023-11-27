1d20698ed1c454fd7728200ab7aec3b40cfbc80e
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2023-11-27 18:07:53 +0000

    Delete TestInterface7.py

--- TestInterface7.py
+++
@@ -1,9 +0,0 @@
-from apysc import Stage, Sprite, display_on_jupyter
-
-window:Stage = Stage(stage_width=1176, stage_height=662, background_color="#000000")
-
-sprite1 = Sprite()
-sprite1.graphics.begin_fill(color="#fff")
-sprite1.graphics.draw_rect(x=50, y=50, width=50, height=50)
-
-display_on_jupyter()
